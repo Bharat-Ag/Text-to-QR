8e5c34fd625f28ca360a615a3a58fcf8427baf3b
Your NameBharar <bharatagrawal2k@gmail.com>
2022-12-06 20:24:08 +0530

    w

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let qrImg = container.querySelector(".qr img");
 
 getBtn.addEventListener('click', () => {
     let inputVale = getInput.value;
-    // if (!inputVale) return;
+    if (!inputVale) return;
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputVale}`;
     container.classList.add("active");
 })
@@ -18,7 +18,7 @@ getInput.addEventListener('keypress', function (e) {
     if (e.key === "Enter") {
         e.preventDefault();
         let inputVale = getInput.value;
-        // if (!inputVale) return;
+        if (!inputVale) return;
         qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputVale}`;
         container.classList.add("active");
 
