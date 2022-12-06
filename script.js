9abcc1c15566728ef433973b2ec283c3386abe8b
Your NameBharar <bharatagrawal2k@gmail.com>
2022-12-06 16:09:24 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let container = document.querySelector(".container"),
+    getInput = container.querySelector(".field #message"),
+    getBtn = container.querySelector(".field .code-btn"),
+    resetBtn = container.querySelector(".field .reset-btn");
+
+let qrImg = container.querySelector(".qr img");
+
+
+getBtn.addEventListener('click', () => {
+    let inputVale = getInput.value;
+    if (!inputVale) return;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputVale}`;
+    container.classList.add("active");
+})
+
+
+
+resetBtn.addEventListener('click', () => {
+    getInput.value = null;
+})
